52b8032b48cec7c2602f54c7c3103f37d2b702e5
Bernhard Lukassen <bl@bernhard-lukassen.com>
2019-11-18 15:51:07 +0100

    basic PubSub implementation

--- index.mjs
+++ index.mjs
@@ -3,19 +3,25 @@
  */
 import Matter from "./lib/matter.mjs";
 
-const handle = () => universe || global || window;
+const space = () => {
+    // this is a cryptic way to get the 'global' object or 'window' in strict mode. direct code references will throw an error
+    const space = (1,eval)("this");
+    return space.universe ? space.universe : space;
+};
+
+//**** now define all standard exports
 
 export { default as Matter }        from './lib/matter.mjs';
 
 export const service = {
     install() {
         console.log('** matter install()');
-        handle().matter = new Matter();
+        space().matter = new Matter();
     },
 
     uninstall() {
         console.log('** matter uninstall()');
-        delete handle().matter;
+        delete space().matter;
     },
 
     resolve() {
@@ -25,12 +31,12 @@ export const service = {
 
     start() {
         console.log('** matter start()');
-        handle().matter.condense();
+        space().matter.condense();
     },
 
     stop() {
         console.log('** matter stop()');
-        handle().matter.freeze();
+        space().matter.freeze();
     },
 
     update() {
@@ -39,4 +45,4 @@ export const service = {
         this.uninstall();
         this.install();
     }
-}
+};
