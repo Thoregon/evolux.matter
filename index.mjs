5c4cb87a91ba662b22009c5028fde710b919b7c8
Bernhard Lukassen <bl@bernhard-lukassen.com>
2019-11-18 16:50:18 +0100

    get rid of unneccessary EventEmitter id, fixes

--- index.mjs
+++ index.mjs
@@ -3,11 +3,11 @@
  */
 import Matter from "./lib/matter.mjs";
 
-const space = () => {
+const myuniverse = (() => {
     // this is a cryptic way to get the 'global' object or 'window' in strict mode. direct code references will throw an error
     const space = (1,eval)("this");
     return space.universe ? space.universe : space;
-};
+})();
 
 //**** now define all standard exports
 
@@ -16,12 +16,12 @@ export { default as Matter }        from './lib/matter.mjs';
 export const service = {
     install() {
         console.log('** matter install()');
-        space().matter = new Matter();
+        myuniverse.matter = new Matter();
     },
 
     uninstall() {
         console.log('** matter uninstall()');
-        delete space().matter;
+        delete myuniverse.matter;
     },
 
     resolve() {
@@ -31,12 +31,12 @@ export const service = {
 
     start() {
         console.log('** matter start()');
-        space().matter.condense();
+        myuniverse.matter.condense();
     },
 
     stop() {
         console.log('** matter stop()');
-        space().matter.freeze();
+        myuniverse.matter.freeze();
     },
 
     update() {
